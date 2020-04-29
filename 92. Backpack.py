1fe6f1abf8f6172729dbc4c08cfb366cc7d0bce7
Althea <lhliu2@illinois.edu>
2020-04-28 22:25:11 -0500

    Update 92. Backpack.py

--- 92. Backpack.py
+++ 92. Backpack.py
@@ -11,7 +11,7 @@ class Solution:
         dp[0][0] = True
         for i in range(1, n + 1):
             for j in range(0, m + 1):
-                dp[i][j] = dp[i-1][j
+                dp[i][j] = dp[i-1][j]
                 if (j >= A[i - 1] and dp[i-1][j - A[i-1]]):
                     dp[i][j] = True
         print(dp)
