2864c9602c303567f8e8c9cfcd448dd2207e9def
Althea <lhliu2@illinois.edu>
2020-04-28 23:17:14 -0500

    Update 563. Backpack V.py

--- 563. Backpack V.py
+++ 563. Backpack V.py
@@ -1,3 +1,23 @@
+class Solution:
+    """
+    @param nums: an integer array and all positive numbers
+    @param target: An integer
+    @return: An integer
+    """
+    def backPackV(self, nums, target):
+        # write your code here
+        n = len(nums)
+        dp = [[0] * (target + 1) for _ in range(n + 1)]
+        dp[0][0] = 1
+        for i in range(1, n + 1):
+            for j in range(0, target + 1):
+                dp[i][j] = dp[i - 1][j]
+                if j >= nums[i - 1]:
+                    dp[i][j] += dp[i - 1][j - nums[i - 1]]
+        return dp[n][target]
+    
+    
+
 public class Solution {
     /**
      * @param nums: an integer array and all positive numbers
